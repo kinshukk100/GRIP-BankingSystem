e1b91e67dddcc126ac16b20097630e3f1488e91e
Kinshuk Kumar <62995366+kinshukk100@users.noreply.github.com>
2021-07-21 00:27:21 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin-amit:Test123@clustersparks1.ivp3h.mongodb.net/bankUser",
+mongoose.connect("mongodb+srv://admin1:Test123@cluster0.yhu9x.mongodb.net/bankUser",
 {useNewUrlParser:true}).then(() => {
   console.log('Database connection successful');
 }).catch( (e) => {
